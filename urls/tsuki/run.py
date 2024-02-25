12e73c67cdb96a1f383999035e487ed7761ed3f8
OneDefauter <onedefauter@gmail.com>
2024-02-25 19:04:56 -0300

    Fix

--- urls/tsuki/run.py
+++ urls/tsuki/run.py
@@ -47,9 +47,6 @@ async def run(driver, url, numero_capitulo, session, folder_selected, nome_foler
         print("Capítulo aguardando aprovação.")
         return 4
     
-    links_das_imagens = []
-    count = 0
-    
     # Espera o leitor carregar
     while True:
         try:
@@ -61,129 +58,92 @@ async def run(driver, url, numero_capitulo, session, folder_selected, nome_foler
         pop_up = driver.find_element(By.CLASS_NAME, 'el-dialog__close')
         pop_up.click()
     except:
-        ...
+        pass
 
-
-    def button_mode(search, define):
-        max_attempts = 3
-        attempts = 0
-
-        while attempts < max_attempts:
-            try:
-                # Aguarde até que o botão esteja presente na página usando XPath com base no texto
-                xpath_expression = f"//button[@class='buttonreader pointer' and text()='{search}']"
-                button_element = WebDriverWait(driver, 3).until(
-                    EC.presence_of_element_located((By.XPATH, xpath_expression))
-                )
-
-                # Verifique o texto do botão
-                button_text = button_element.text
-
-                if button_text == search:
-                    button_element.click()
-                    
-                    time.sleep(2)
-
-                    try:
-                        # Aguarde até que o botão esteja presente na página usando XPath com base no texto
-                        xpath_expression = f"//button[@class='buttonreader pointer' and text()='{define}']"
-                        button_element = WebDriverWait(driver, 5).until(
-                            EC.presence_of_element_located((By.XPATH, xpath_expression))
-                        )
-                    except:
-                        # Incrementar o contador de tentativas se o tempo de espera exceder
-                        attempts += 1
-                        continue
-                    
-                    else:
-                        break
-
-            except:
-                # Incrementar o contador de tentativas se o tempo de espera exceder
-                attempts += 1
-                continue
-
-    button_mode('Passar páginas', 'Páginas abertas')
-
-    # Descobrir a quantidade de paginas
-    elemento_lista_paginas = driver.find_element(By.CSS_SELECTOR, 'div[id^="el-popper-container-"] div:nth-child(2) div div div:nth-child(1) ul')
-    itens_lista_paginas = elemento_lista_paginas.find_elements(By.CSS_SELECTOR, 'li')
-    numero_ultima_pagina = int(len(itens_lista_paginas))
-
-    button_mode('Páginas abertas', 'Passar páginas')
-
+    try:
+        botao = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[text()='Páginas abertas']")) 
+        )
+        botao.click()
+    except:
+        pass
+    
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2);")
+
+    time.sleep(1)
 
     def load_images():
         count_repet = 0
         count_limit = 10
-        
+
         while count_repet < count_limit:
-            
-            leitor = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "manga-reader"))
-            )
+            try:
+                if debug_var.get():
+                    baixando_label.config(text=f"Carregando capítulo {numero_capitulo}\nVerificação {count_repet + 1} / {count_limit}")
 
-            imagens = leitor.find_elements(By.TAG_NAME, 'img')
+                # Espera até que o elemento do leitor esteja presente na página
+                leitor = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "manga-reader"))
+                )
 
-            # Função para rolar até a imagem e aguardar o carregamento
-            def scroll_to_image(image_element):
-                driver.execute_script("arguments[0].scrollIntoView();", image_element)
-                wait_time = 0
-                while not image_element.get_attribute("complete") and wait_time < 10:
-                    time.sleep(0.5)
-                    wait_time += 1
-                
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # Obtém todas as imagens dentro do leitor
+                paginas = leitor.find_elements(By.TAG_NAME, 'img')
 
-            # Itera sobre as imagens
-            for imagem in imagens:
-                scroll_to_image(imagem)
+                # Função para rolar até a imagem e aguardar o carregamento
+                def scroll_to_image(image_element):
+                    driver.execute_script("arguments[0].scrollIntoView();", image_element)
+                    wait_time = 0
+                    while not image_element.get_attribute("complete") and wait_time < 10:
+                        time.sleep(0.5)
+                        wait_time += 1
 
-            count_repet += 1
-            
-        return imagens
+                # Itera sobre as imagens
+                for imagem in paginas:
+                    scroll_to_image(imagem)
 
-    imagens = load_images()
+                count_repet += 1
 
+            except Exception as e:
+                print(f"Erro durante o carregamento de imagens: {e}")
+                # Se ocorrer um erro, você pode querer tentar novamente ou lidar com a situação de outra maneira
+    
+    load_images()
+    
     while True:
-        if len(imagens) != numero_ultima_pagina:
-            imagens = load_images()
+        # Espera até que o elemento do leitor esteja presente na página
+        leitor = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "manga-reader"))
+        )
+
+        # Obtém todas as imagens dentro do leitor
+        paginas = leitor.find_elements(By.TAG_NAME, 'img')
+        links_das_imagens = []
+
+        # Função para rolar até a imagem e aguardar o carregamento
+        def scroll_to_image(image_element):
+            driver.execute_script("arguments[0].scrollIntoView();", image_element)
+            wait_time = 0
+            while not image_element.get_attribute("complete") and wait_time < 10:
+                time.sleep(0.5)
+                wait_time += 1
+
+        # Itera sobre as imagens
+        for imagem in paginas:
+            scroll_to_image(imagem)
+            
+            # Aqui você pode adicionar o código para baixar a imagem ou qualquer outra ação desejada
+            links_das_imagens.append(str(imagem.get_attribute('src')))
+            
+        # Extrai os links das imagens
+        links_das_imagens = [link.strip() for link in links_das_imagens]
+        links_count = str(len(links_das_imagens))
+        
+        if 'None' in links_das_imagens:
+            load_images()
         else:
             break
-
-        while True:
-            leitor = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "manga-reader"))
-            )
-        
-            paginas = driver.find_elements(By.XPATH, '//img[@class="pagereader" and @alt="Manga page"]')
-            links_das_imagens = []
-        
-            # Função para rolar até a imagem e aguardar o carregamento
-            def scroll_to_image(image_element):
-                driver.execute_script("arguments[0].scrollIntoView();", image_element)
-                wait_time = 0
-                while not image_element.get_attribute("complete") and wait_time < 10:
-                    time.sleep(1)
-                    wait_time += 1
-        
-            # Itera sobre as imagens
-            for imagem in paginas:
-                scroll_to_image(imagem)
-                
-                # Aqui você pode adicionar o código para baixar a imagem ou qualquer outra ação desejada
-                links_das_imagens.append(str(imagem.get_attribute('src')))
-            
-            # Extrai os links das imagens
-            links_das_imagens = [link.strip() for link in links_das_imagens]
-            links_count = str(len(links_das_imagens))
-            
-            if 'None' in links_das_imagens:
-                load_images()
-            else:
-                break
-            
+    
+    
     def download_images(count, files):
         for imagem in links_das_imagens:
             if debug_var.get():
@@ -211,66 +171,52 @@ async def run(driver, url, numero_capitulo, session, folder_selected, nome_foler
             )
             download_button.click()
             
-            # Remover parâmetros de consulta da URL
-            parsed_url = urlparse(imagem)
-            url_without_query = parsed_url.scheme + "://" + parsed_url.netloc + parsed_url.path
-
-            # Encontrar a extensão do arquivo
-            extension_match = re.search(r'\.(jpg|jpeg|png|gif|bmp|webp|avif)$', url_without_query, re.IGNORECASE)
-
+            extension_match = re.search(r'\.(jpg|jpeg|png|gif|bmp|webp)$', imagem, re.IGNORECASE)
+            
             if extension_match:
                 file_extension = extension_match.group(1)
             
             print(f"{Fore.GREEN}Baixando {imagem} como {count:02d}.{file_extension}...{Style.RESET_ALL}")
             
+            time.sleep(2)
+            
             attention = 0
+            warning_img = 0
             while True:
                 lista = os.listdir(download_folder)
-                files_with_tmp_or_cdownload_extension = [file for file in os.listdir(download_folder) if file.endswith('.tmp') or file.endswith('.cdownload')]
-                
-                if files_with_tmp_or_cdownload_extension:
-                    if attention < 99999:
+                if len(lista) > files:
+                    files += 1
+                    break
+                else:
+                    if warning_img > 3:
+                        print(f"{Fore.RED}Falha ao baixar {imagem} como {count:02d}.{file_extension}...{Style.RESET_ALL}")
+                        break
+                    elif attention < 1000:
                         attention += 1
+                        time.sleep(0.1)
                         continue
                     else:
                         download_button.click()
                         attention = 0
+                        warning_img += 1
                         continue
-                
-                if len(os.listdir(download_folder)) != files:
-                    files += 1
-                    break
             
             count += 1
-        
+            
         return count
     
-    count = download_images(1, 0)
+    download_images(1, 0)
     
     if debug_var.get():
         baixando_label.config(text=f"Arrumando páginas...")
-        
-    time.sleep(2)
     
-    while True:
-        lista = os.listdir(download_folder)
-        if count == len(lista) + 1:
-            break
-        
-        else:
-            for image in lista:
-                image = os.path.join(download_folder, image)
-                os.remove(image)
-            
-            count = download_images(1, 0)
-
     move.setup(download_folder, folder_path)
             
-    app_instance.move_text_wait(f'Capítulo {numero_capitulo} baixado com sucesso')
-    
     organizar.organizar(folder_path, compactar, compact_extension, extension)
 
-    print(f"═══════════════════════════════════► {nome} -- {numero_capitulo} ◄═══════════════════════════════════════\n")
-    
-    return 0
+    if debug_var.get():
+        baixando_label.config(text=f"Aguarde...")
+        
+    app_instance.move_text_wait(f'Capítulo {numero_capitulo} baixado com sucesso')
 
+    print(f"═══════════════════════════════════► {nome} -- {numero_capitulo} ◄═══════════════════════════════════════\n")
